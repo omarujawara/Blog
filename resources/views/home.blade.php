5274e53b86797f179564890cd7b7d4d941a0f846
Omru Jawara <omaru.jawara94@gmail.com>
2020-09-24 21:40:12 -0400

    Add model relationship

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,35 @@
                         </div>
                     @endif
                     <a href="./posts/create" class="btn btn-primary"> New Post! </a><br /> 
-                    {{ __('You are logged in!') }}
+                    <p>{{ __('Your Blog Post(s)') }}</p>
+                    @if (count($posts) > 0)
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Title</th>
+                                    <th>Edit</th>
+                                    <th>Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($posts as $post)
+                                <tr>
+                                    <td>{{$post->title}}</td>
+                                    <td><a href="/posts/{{$post->id}}" class="btn btn-default">Edit</a></td>
+                                    <td style="color:red;">
+                                        {!! Form::open(['action'=> ['PostController@destroy',$post->id],'method' => 'POST','class' => 'pull-right'])  !!}
+                                            {{Form::hidden('_method', 'DELETE')}}
+                                            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                        {!!Form::close() !!}
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+
+                        </table>
+                    @else
+                        {{__('You have no post')}}
+                    @endif
                 </div>
             </div>
         </div>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,7 @@
         @foreach ($posts as $post)
             <div class="card mt-3 p-3">
                 <h3><a href="./posts/{{$post->id}}">{{$post->title}}</a></h3>
-                <small>Written on {{$post->created_at}}</small>
+                <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
             </div>       
         @endforeach
     @else 
